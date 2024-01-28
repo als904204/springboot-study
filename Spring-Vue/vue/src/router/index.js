797eb463957c48803824e1678be491370ae62e1d
Min <als904204@naver.com>
2024-01-28 17:30:53 +0900

    add /home router

--- Spring-Vue/vue/src/router/index.js
+++ Spring-Vue/vue/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PostList from '@/components/PostList.vue';
 import PostDetails from '@/components/PostDetails.vue';
+import Home from "@/components/Home.vue";
 
 const routes = [
-  { path: '/posts', component: PostList },
-  { path: '/posts/:id', component: PostDetails, name: 'PostDetails' },
+  {path: '/', component: Home},
+  {path: '/posts', component: PostList},
+  {path: '/posts/:id', component: PostDetails, name: 'PostDetails'},
 ];
 
 const router = createRouter({
