f8024f570fbd36247cd7e6e0f3eda813a39f5a6e
Min <als904204@naver.com>
2024-01-28 17:50:49 +0900

    router 적용

--- Spring-Vue/vue/src/router/index.js
+++ Spring-Vue/vue/src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PostList from '@/components/PostList.vue';
 import PostDetails from '@/components/PostDetails.vue';
 import Home from "@/components/Home.vue";
+import Naver from "@/components/webtoon/Naver.vue";
 
 const routes = [
   {path: '/', component: Home},
+  {path: '/naver', component: Naver},
   {path: '/posts', component: PostList},
   {path: '/posts/:id', component: PostDetails, name: 'PostDetails'},
 ];
