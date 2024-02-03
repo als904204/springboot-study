312d297a5686a39039cd46e14b83a86301cddc59
Min <als904204@naver.com>
2024-02-03 16:23:28 +0900

    OAuth2 User DB Save

--- oauth2/spring-security-oauth2-session/src/main/java/com/example/springsecurityoauth2session/service/CustomOAuth2Service.java
+++ oauth2/spring-security-oauth2-session/src/main/java/com/example/springsecurityoauth2session/service/CustomOAuth2Service.java
@@ -1,7 +1,10 @@
 package com.example.springsecurityoauth2session.service;
 
+import com.example.springsecurityoauth2session.dto.CustomOAuth2User;
 import com.example.springsecurityoauth2session.dto.NaverResponse;
 import com.example.springsecurityoauth2session.dto.OAuth2Response;
+import com.example.springsecurityoauth2session.entiy.UserEntity;
+import com.example.springsecurityoauth2session.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,6 +14,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomOAuth2Service extends DefaultOAuth2UserService {
 
+    private final UserRepository userRepository;
+
+    public CustomOAuth2Service(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -29,7 +37,34 @@ public class CustomOAuth2Service extends DefaultOAuth2UserService {
             throw new IllegalAccessError("잘못된 Oauth2요청 ");
         }
 
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
 
-        return super.loadUser(userRequest);
+        String role = "ROLE_USER";
+
+        // 첫 로그인
+        if (existData == null) {
+
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole(role);
+
+            userRepository.save(userEntity);
+        }
+
+        // 이미 가입된 경우
+        else {
+
+            existData.setUsername(username);
+            existData.setEmail(oAuth2Response.getEmail());
+
+            role = existData.getRole();
+
+            userRepository.save(existData);
+        }
+
+        return new CustomOAuth2User(oAuth2Response, role);
     }
 }
+
