1eae8b67bf24266358c0e4ec592dccc1c93ca695
Min <als904204@naver.com>
2024-02-03 15:37:47 +0900

    Spring Security Oauth2 Session init

---
+++ oauth2/spring-security-oauth2-session/src/main/java/com/example/springsecurityoauth2session/SpringSecurityOauth2SessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityoauth2session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2SessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2SessionApplication.class, args);
+    }
+
+}
