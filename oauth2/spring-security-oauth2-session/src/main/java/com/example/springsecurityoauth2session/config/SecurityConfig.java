d873245b617c2908f199c49e37726eb5e62e031c
Min <als904204@naver.com>
2024-02-03 16:46:12 +0900

    커스텀 Registration 적용 후,리다이렉션 적용

--- oauth2/spring-security-oauth2-session/src/main/java/com/example/springsecurityoauth2session/config/SecurityConfig.java
+++ oauth2/spring-security-oauth2-session/src/main/java/com/example/springsecurityoauth2session/config/SecurityConfig.java
@@ -1,21 +1,24 @@
 package com.example.springsecurityoauth2session.config;
 
+import com.example.springsecurityoauth2session.oauth2.CustomClientRegistrationRepository;
 import com.example.springsecurityoauth2session.service.CustomOAuth2Service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2Service customOAuth2Service;
-
-    public SecurityConfig(CustomOAuth2Service customOAuth2Service) {
+    private final CustomClientRegistrationRepository clientRegistrationRepository;
+    public SecurityConfig(CustomOAuth2Service customOAuth2Service,
+        CustomClientRegistrationRepository clientRegistrationRepository) {
         this.customOAuth2Service = customOAuth2Service;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     @Bean
@@ -29,8 +32,10 @@ public class SecurityConfig {
         http
             .oauth2Login((oauth2) -> oauth2
                 .loginPage("/customLogin")
+                .clientRegistrationRepository(clientRegistrationRepository.clientRegistrationRepository())
                 .userInfoEndpoint(config ->
-                    config.userService(customOAuth2Service)));
+                    config.userService(customOAuth2Service))
+                .successHandler(new SimpleUrlAuthenticationSuccessHandler("/success")));
         http
             .authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/", "/oauth2/**", "/customLogin/**").permitAll()
