f870ff9bd67038982a2005f03547fd92ec5176f2
Min <als904204@naver.com>
2024-03-04 13:41:50 +0900

    ngrinder init

---
+++ algorithm/src/main/java/com/example/algorithm/ch02/LinearSearch.java
@@ -0,0 +1,6 @@
+package com.example.algorithm.ch02;
+
+public class LinearSearch {
+
+    
+}
---
+++ algorithm/src/test/java/com/example/algorithm/Ex.java
@@ -0,0 +1,17 @@
+package com.example.algorithm;
+
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
+public class Ex {
+
+    @Test
+    void main() {
+
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        System.out.println("i = " + i);
+    }
+
+}
