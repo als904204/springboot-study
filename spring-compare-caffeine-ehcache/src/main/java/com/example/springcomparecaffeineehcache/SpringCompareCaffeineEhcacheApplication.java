1f4b4d0e3e60d67f4779192502c93b4596d5df23
Min <als904204@naver.com>
2024-03-08 01:16:38 +0900

    cache비교 project init

---
+++ spring-compare-caffeine-ehcache/src/main/java/com/example/springcomparecaffeineehcache/SpringCompareCaffeineEhcacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcomparecaffeineehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCompareCaffeineEhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCompareCaffeineEhcacheApplication.class, args);
+	}
+
+}
---
+++ spring-compare-caffeine-ehcache/src/test/java/com/example/springcomparecaffeineehcache/SpringCompareCaffeineEhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcomparecaffeineehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompareCaffeineEhcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
