20b4fceb260c21859213ecbc89ce4ef4a8151beb
Min <als904204@naver.com>
2024-05-07 16:47:40 +0900

    테스트용 domain

---
+++ spring-web-socket/src/main/java/org/example/springwebsocket/domain/Greeting.java
@@ -0,0 +1,19 @@
+package org.example.springwebsocket.domain;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+}
