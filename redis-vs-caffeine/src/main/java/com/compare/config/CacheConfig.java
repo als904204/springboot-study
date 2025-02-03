c3902ed9666ccd193f6525bbe9e19b7c5e018fba
Min <als904204@naver.com>
2025-02-03 18:05:40 +0900

    환경별 redis, caffeine 캐시 빈 설정

---
+++ redis-vs-caffeine/src/main/java/com/compare/config/CacheConfig.java
@@ -0,0 +1,70 @@
+package com.compare.config;
+
+import java.time.Duration;
+import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class CacheConfig {
+
+    @Value("${cache.expiration:10}")
+    private long expiration;
+
+    private final Environment env;
+
+    @PostConstruct
+    public void currentProfile() {
+        String[] activeProfiles = env.getActiveProfiles();
+        if (activeProfiles.length == 0) {
+            activeProfiles = env.getDefaultProfiles();
+        }
+        log.info("Active Profiles : {}", Arrays.toString(activeProfiles));
+    }
+
+
+    /**
+     * Redis
+     * application-redis.yml
+     */
+    @Bean
+    @ConditionalOnProperty(name = "cache.type", havingValue = "redis")
+    public CacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofSeconds(expiration))
+            .disableCachingNullValues()
+            .serializeValuesWith(RedisSerializationContext.SerializationPair
+                .fromSerializer(new org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.builder(connectionFactory)
+            .cacheDefaults(cacheConfiguration)
+            .build();
+    }
+
+    /**
+     * Caffeine
+     * application-caffeine.yml
+     */
+    @Bean
+    @ConditionalOnProperty(name = "cache.type", havingValue = "caffeine", matchIfMissing = true)
+    public CacheManager caffeineCacheManager() {
+        org.springframework.cache.caffeine.CaffeineCacheManager cacheManager = new org.springframework.cache.caffeine.CaffeineCacheManager("posts");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofSeconds(expiration))
+            .maximumSize(100));
+        return cacheManager;
+    }
+}
