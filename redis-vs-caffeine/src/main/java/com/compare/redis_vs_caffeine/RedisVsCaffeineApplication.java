b1669f5c8ec50d4930e9e46353d062d335044cdb
Min <als904204@naver.com>
2025-02-03 17:35:06 +0900

    init redis-vs-caffeiene proejct

---
+++ redis-vs-caffeine/src/main/java/com/compare/redis_vs_caffeine/RedisVsCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.compare.redis_vs_caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisVsCaffeineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisVsCaffeineApplication.class, args);
+	}
+
+}
---
+++ redis-vs-caffeine/src/test/java/com/compare/redis_vs_caffeine/RedisVsCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compare.redis_vs_caffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisVsCaffeineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
