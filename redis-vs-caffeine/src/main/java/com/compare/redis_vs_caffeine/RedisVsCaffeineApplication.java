2e8f2beb206e61267e60ab123a64311192ff5b52
Min <als904204@naver.com>
2025-02-03 18:05:49 +0900

    캐시 활성화

--- redis-vs-caffeine/src/main/java/com/compare/redis_vs_caffeine/RedisVsCaffeineApplication.java
+++ redis-vs-caffeine/src/main/java/com/compare/redis_vs_caffeine/RedisVsCaffeineApplication.java
@@ -2,8 +2,10 @@ package com.compare.redis_vs_caffeine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisVsCaffeineApplication {
 
 	public static void main(String[] args) {
